68df13a6a89bd6273a07d8c3157f46126e4599ce
AchrafSaid <ashraf.said@msa.edu.eg>
2026-04-08 01:54:47 +0200

    Some Updates

--- index.cpp
+++ index.cpp
@@ -9,9 +9,57 @@ void printBoard(char Board[10][10]){
         std::cout<<std::endl;
     }
 }
+void VerifyPostions(std::string postion,std::string move,std::int row,std::int col){
+    if(postion[0]=='a')
+        col=1;
+    if(postion[0]=='b')
+        col=2
+    if(position[0]=='c')
+        col=3;
+    if(postion[0]=='d')
+        col=4;
+    if(postion[0]=='e')
+        col=5;
+    if(postion[0]=='f')
+        col=6;
+    if(postion[0]=='g')
+        col=7;
+    if(postion[0]=='h')
+        col=8;
+    if(postion[1]=='1')
+        row=8;
+    if(postion[1]=='2')     
+        row=7;
+    if(postion[1]=='3')
+        row=6;
+    if(postion[1]=='4')
+        row=5;
+    if(postion[1]=='5')
+        row=4;
+    if(postion[1]=='6')
+        row=3;
+    if(postion[1]=='7')
+        row=2;
+    if(postion[1]=='8')
+        row=1;
+}
+char Whatpiece(char piece){
+    if(piece=='P')
+        return 'P';
+    if(piece=='R')
+        return 'R';
+    if(piece=='k')
+        return 'k';
+    if(piece=='B')
+        return 'B';
+    if(piece=='Q')
+        return 'Q';
+    if(piece=='K')
+        return 'K';
+}
 int main(){
     char Board[10][10]={
-        {' ','a','b','c','d','e','f','g','h'},
+        {' ','a','b','c','d','e','f','g','h',' '},
         {'8','R','N','B','Q','K','B','N','R','8'},
         {'7','P','P','P','P','P','P','P','P','7'},
         {'6',' ',' ',' ',' ',' ',' ',' ',' ','6'},
@@ -26,10 +74,16 @@ int main(){
     printBoard(Board);
     std::string postion;
     std::string move;
+    std::int row=0;
+    std::int col=0;
     while(true){        
         std::cout<<"Enter The Postion of the piece that you want to move: ";
         std::cin>>postion;
         std::cout<<"Where to move: ";
+        std::cin>>move;
+        row=0,col=0;
+        VerifyPostions(postion,move,row,col);
+        char piece = WhatPiece(Board[row][col]);
     }
     return 0;
 
